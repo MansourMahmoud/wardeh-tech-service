d539887aa60f08416f51ff913a62f931872082d3
MansourMahmoud <mansourmahmoud77a@gmail.com>
2024-03-09 16:20:22 +0200

    fix

--- src/utils/navListItem.js
+++ src/utils/navListItem.js
@@ -1,15 +1,15 @@
 //   variable list for content
 export const navListItem = [
-  {
-    name: "عرض أسعار",
-    goTo: "/",
-  },
-  {
-    name: "باقات حصرية",
-    goTo: "/packages-deals",
-  },
-  {
-    name: "عرض أسعار الباقات",
-    goTo: "/packages-deals-form",
-  },
+  // {
+  //   name: "عرض أسعار",
+  //   goTo: "/",
+  // },
+  // {
+  //   name: "باقات حصرية",
+  //   goTo: "/packages-deals",
+  // },
+  // {
+  //   name: "عرض أسعار الباقات",
+  //   goTo: "/packages-deals-form",
+  // },
 ];
